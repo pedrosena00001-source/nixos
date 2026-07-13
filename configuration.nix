f6f6ebaaa4bdca5fe83ae2a9c7eac751678b36d5
pedrosena00001-source <pedro.sena00001@gmail.com>
2026-07-13 07:40:04 +0000

    configuration

---
+++ configuration.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix 
+      ./packages.nix
+    ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "nixos-btw"; 
+  networking.networkmanager.enable = true;
+  time.timeZone = "America/Sao_Paulo";
+  services.xserver = {
+         enable = true;
+         autoRepeatDelay = 200;
+         autoRepeatInterval = 35;
+};
+  services.displayManager.ly.enable = true;
+  programs.hyprland.enable = true;
+  services.xserver.xkb.layout = "br";
+  services.xserver.xkb.variant = "abnt2";
+  services.xserver.xkb.options = "eurosign:e,caps:escape";
+  users.users.pedro = {
+    isNormalUser = true;
+   extraGroups = [ "wheel" "docker" ]; 
+    packages = with pkgs; [
+      tree
+    ];
+  };
+  programs.zsh.enable = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  services.flatpak.enable = true;
+  users.users.pedro.shell = pkgs.zsh;
+  services.openssh = {
+  		enable = true;
+		settings.PasswordAuthentication = false;
+	};
+  system.stateVersion = "26.05";
+
+}
+
