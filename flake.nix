8c9934e308e73af3ef546cb0511d0852fc6b1e32
pedrosena00001-source <pedro.sena00001@gmail.com>
2026-07-13 08:20:37 +0000

    flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+    description ="Nixos from Scratch"; 
+    inputs = {
+           nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	   home-manager = {
+                      url ="github:nix-community/home-manager/release-25.05";
+                      inputs.nixpkgs.follows = "nixpkgs";
+            };
+    };
+
+   outputs = { self, nixpkgs, home-manager, ... }: {
+       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem{
+            system = "x86_64-linux";
+            modules = [
+                ./configuration.nix
+                home-manager.nixosModules.home-manager
+                {
+                   home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        users.pedro = import ./home.nix;
+                        backupFileExtension = "backup";
+                      };
+                    } 
+                  ]; 
+                };
+              };
+}
