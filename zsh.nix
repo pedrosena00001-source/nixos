a8cd79bf8e3d4585b225c187ab6bffe3eadbece0
pedrosena00001-source <pedro.sena00001@gmail.com>
2026-07-17 00:15:13 -0300

    zsh.nix adicionado

---
+++ zsh.nix
@@ -0,0 +1,44 @@
+{ config, lib, pkgs, ... }:
+{
+      programs.zsh = {
+          enable = true;
+          autosuggestions.enable = true;    
+          syntaxHighlighting.enable = true; 
+          enableCompletion = true;
+  oh-my-zsh = {
+    enable = true;
+    theme = "gnzh";
+    plugins = [
+        "git"
+        "sudo" 
+        "history"
+        "docker"
+	];
+  };
+  plugins = [
+  		{                              
+	          name = "vi-mode";
+                  src = pkgs.zsh-vi-mode;
+	          file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+	  }
+	];
+          shellAliases = {
+                 ll = "ls -la";
+                 sv = "nvim";
+                 up = "sudo nixos-rebuild switch --flake .#nixos";
+                 v = "vim";
+                 garbage = "sudo nix-collect-garbage -d";
+                 cdd = "cd nixos-dotfiles";
+                 gm = "git commit -m";
+                 ga = "git add";
+                 gs = "git status";
+                 gp = "git push origin main";
+                 gpm = "git push origin master";
+                 ns = "nix-shell -p";
+                 sh = "shutdown -h now";
+ 		 re = "reboot";
+                 gaa = "git add -A";
+		 sd = "docker";
+          };
+    };
+}
