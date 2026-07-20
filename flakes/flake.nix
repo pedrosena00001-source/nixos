9189928fcbf8b091fb52e2f0aa6a409700f0dc1f
pedrosena00001-source <pedro.sena00001@gmail.com>
2026-07-20 15:36:54 -0300

    pasta propria para arquivos flakes

---
+++ flakes/flake.nix
@@ -0,0 +1,28 @@
+{
+    description = "Nixos from Scratch"; 
+    inputs = {
+           nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	  home-manager = {
+                      url = "github:nix-community/home-manager/master";
+                      inputs.nixpkgs.follows = "nixpkgs";
+            };
+    };
+   outputs = { self, nixpkgs, home-manager, ... }: {
+       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                ./configuration.nix
+                home-manager.nixosModules.home-manager
+                {
+                   home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        backupFileExtension = "backup";
+                        extraSpecialArgs = { inherit self; };
+                        users.pedro = import ./home.nix;
+                      };
+                    } 
+                  ]; 
+                };
+              };
+}
