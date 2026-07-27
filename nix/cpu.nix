6b01dbfd079efb31c8713536fb067231dea7a06c
pedrosena00001-source <pedro.sena00001@gmail.com>
2026-07-27 00:54:02 -0300

    serviço de controle da plano de energia do meu notebook

---
+++ nix/cpu.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+{
+services.auto-cpufreq.enable = true;
+services.auto-cpufreq.settings = {
+  battery = {
+    governor = "powersave";
+    turbo = "never";
+  };
+  charger = {
+    governor = "performance";
+    turbo = "auto";
+  };
+};
+}
